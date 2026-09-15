c1f3cc4cd10fa571967df39b05242e5a37c42a4b
grimboso <grim_p2@hotmail.com>
2026-09-15 10:17:44 +0200

    PCM Buff Bars: defer restricted viewer refreshes

--- Modules/PCM/PUI_PCM_BuffBarViewer.lua
+++ Modules/PCM/PUI_PCM_BuffBarViewer.lua
@@ -417,6 +417,10 @@ local function _Refresh()
     return
   end
 
+  if PCMHooks.IsAddonRestricted() then
+    return
+  end
+
   local viewer = PCMRuntime:GetViewer("BuffBarCooldownViewer")
   if not viewer or viewer:IsForbidden() then
     _Init()
